b4801158108fabf24033c5e9120e8705781aa98d
liuzhuoling <liuzhuoling@mycapital.net>
2017-04-13 21:18:43 +0800

    speed up && add main.cpp

--- ColorPrint.h
+++ ColorPrint.h
@@ -21,14 +21,7 @@
 		C_BLACK = 0,
 	};
 
-	#define CC_RED setConsoleColor(C_RED)
-	#define CC_GREEN setConsoleColor(C_GREEN)
-	#define CC_BLUE setConsoleColor(C_BLUE)
-	#define CC_YELLOW setConsoleColor(C_YELLOW)
-	#define CC_CYAN setConsoleColor(C_CYAN)
-	#define CC_BLACK setConsoleColor(C_BLACK)
-	#define CC_WHITE setConsoleColor(C_WHITE)
-	#define CC_RESET setConsoleColor()
+	static HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	inline char setConsoleColor(ConsoleColor textColor = C_WHITE, ConsoleColor backgroundColor = C_BLACK)
 	{
@@ -37,7 +30,42 @@
 		SetConsoleTextAttribute(handle, textColor | backgroundColor);
 		return '\r';
 	}
-	
+
+	#define CC_RED SetConsoleTextAttribute(handle,C_RED|C_BLACK)
+	#define CC_GREEN SetConsoleTextAttribute(handle,C_GREEN|C_BLACK)
+	#define CC_BLUE SetConsoleTextAttribute(handle,C_BLUE|C_BLACK)
+	#define CC_YELLOW SetConsoleTextAttribute(handle,C_YELLOW|C_BLACK)
+	#define CC_CYAN SetConsoleTextAttribute(handle,C_CYAN|C_BLACK)
+	#define CC_BLACK SetConsoleTextAttribute(handle,C_BLACK|C_BLACK)
+	#define CC_WHITE SetConsoleTextAttribute(handle,C_WHITE|C_BLACK)
+	#define CC_RESET SetConsoleTextAttribute(handle,C_WHITE|C_BLACK)
+
+	#ifndef NoMessage 
+		#define PRINT(level,content) do{\
+			switch (level) {\
+				/* For Success */\
+				case 'S': CC_GREEN; std::cout << "[SUCCESS " << __FUNCTION__ << ":" << __LINE__ << "] " << content << std::endl; CC_RESET; break;\
+				/* For Notice */\
+				case 'N': CC_CYAN;  std::cout << "[NOTICE " << __FUNCTION__ << ":" << __LINE__ << "] " << content << std::endl; CC_RESET; break;\
+				/* For Warn */\
+				case 'W': CC_YELLOW; std::cout << "[WARNNING " << __FUNCTION__ << ":" << __LINE__ << "] " << content << std::endl; CC_RESET; break;\
+				/* For Error */\
+				case 'E': CC_RED; std::cout << "[ERROR " << __FUNCTION__ << ":" << __LINE__ << "] " << content << std::endl; CC_RESET; break;\
+				/* For Normal Info */\
+				case 'I': std::cout << "[INFO " << __FUNCTION__ << ":" << __LINE__ << "] " << content << std::endl; break;\
+				default : break;\
+			}\
+		}while(0)
+	#else
+	#define PRINT(level,content) do{\
+			switch (level) {\
+				/* For Error */\
+				case 'E': CC_RED; std::cout << "[ERROR " << __FUNCTION__ << ":" << __LINE__ << "] " << content << std::endl; CC_RESET; break;\
+				default : break;\
+			}\
+		}while(0)
+	#endif
+
 #else
 	#define CC_RED "\033[31m"
 	#define CC_GREEN "\033[32m"
@@ -47,31 +75,33 @@
 	#define CC_WHITE "\033[37m"
 	#define CC_BLACK "\033[30m"
 	#define CC_RESET "\033[0m"
+
+	#ifndef NoMessage 
+		#define PRINT(level,content) do{\
+			switch (level) {\
+				/* For Success */\
+				case 'S': std::cout << CC_GREEN << "[SUCCESS " << __FUNCTION__ << ":" << __LINE__ << "] " << content << CC_RESET << std::endl; break;\
+				/* For Notice */\
+				case 'N': std::cout << CC_CYAN << "[NOTICE " << __FUNCTION__ << ":" << __LINE__ << "] " << content << CC_RESET << std::endl; break;\
+				/* For Warn */\
+				case 'W': std::cout << CC_YELLOW << "[WARNNING " << __FUNCTION__ << ":" << __LINE__ << "] " << content << CC_RESET << std::endl; break;\
+				/* For Error */\
+				case 'E': std::cout << CC_RED << "[ERROR " << __FUNCTION__ << ":" << __LINE__ << "] " << content << CC_RESET << std::endl; break;\
+				/* For Normal Info */\
+				case 'I': std::cout << "[INFO " << __FUNCTION__ << ":" << __LINE__ << "] " << content << std::endl; break;\
+				default : break;\
+			}\
+		}while(0)
+	#else
+		#define PRINT(level,content) do{\
+			switch (level) {\
+				/* For Error */\
+				case 'E': std::cout << CC_RED << "[ERROR " << __FUNCTION__ << ":" << __LINE__ << "] " << content << CC_RESET << std::endl; break;\
+				default : break;\
+			}\
+		}while(0)
+	#endif
 #endif // _WIN32
 
-#ifndef NoMessage 
-	#define PRINT(level,content) do{\
-		switch (level) {\
-			/* For Success */\
-			case 'S': std::cout << CC_GREEN << "[SUCCESS " << __FUNCTION__ << ":" << __LINE__ << "]" << content << CC_RESET << std::endl; CC_RESET; break;\
-			/* For Notice */\
-			case 'N': std::cout << CC_CYAN << "[NOTICE " << __FUNCTION__ << ":" << __LINE__ << "]" << content << CC_RESET << std::endl; CC_RESET; break;\
-			/* For Warn */\
-			case 'W': std::cout << CC_YELLOW << "[WARNNING " << __FUNCTION__ << ":" << __LINE__ << "]" << content << CC_RESET << std::endl; CC_RESET; break;\
-			/* For Error */\
-			case 'E': std::cout << CC_RED << "[ERROR " << __FUNCTION__ << ":" << __LINE__ << "]" << content << CC_RESET << std::endl; CC_RESET; break;\
-			/* For Normal Info */\
-			case 'I': std::cout << "[INFO " << __FUNCTION__ << ":" << __LINE__ << "]" << content << std::endl; break;\
-			default : break;\
-		}\
-	}while(0)
-#else
-	#define PRINT(level,content) do{\
-		switch (level) {\
-			/* For Error */\
-			case 'E': std::cout << CC_RED << "[ERROR " << __FUNCTION__ << ":" << __LINE__ << "]" << content << CC_RESET << std::endl; CC_RESET; break;\
-			default : break;\
-		}\
-	}while(0)
-#endif
+
 
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "ColorPrint.h"
+
+int main() {
+	PRINT('I', "lalala, I am white!");
+	PRINT('N', "lalala, I am cyan!");
+	PRINT('W', "lalala, I am yellow!");
+	PRINT('S', "lalala, I am green!");
+	PRINT('E', "lalala, I am red!");
+	PRINT('I', "lalala, I am white again!");
+//	system("pause");
+}
